6ddcc173acf8cf11ddcd0240d66c7a2070b5ec48
mmi-dev <m.midoux@hotmail.fr>
2022-05-31 18:37:40 +0200

    refactor: simplify code for creation of options list

--- js/pages/index.js
+++ js/pages/index.js
@@ -13,74 +13,35 @@ async function displayRecipes(recipesList) {
   }
 }
 
-async function displayIngredientsList(recipesList) {
-  const ingredientsFilter = document.querySelector('#ingredients-list')
+async function displayOptions(recipesList) {
+  // ingredients list creation
+  const ingredientsList = await displayOptionsList(
+    recipesList,
+    '#ingredients-list',
+    'ingredients'
+  )
 
-  let ingredients = []
-  // list all ingredients
-  for (let i = 0; i < recipesList.length; i++) {
-    recipesList[i].ingredients.forEach((e) => {
-      ingredients.push(e.ingredient)
-    })
-  }
-  // delete double
-  ingredients = [...new Set(ingredients)]
-  // sort
-  ingredients.sort()
+  // appliance list creation
+  const appliancesList = await displayOptionsList(
+    recipesList,
+    '#appliances-list',
+    'appareils'
+  )
 
-  const ingredientsModel = ingredientsFactory(ingredients)
-  const ingeredientsListDOM = ingredientsModel.getIngredientsListDOM()
-  ingredientsFilter.appendChild(ingeredientsListDOM)
-}
-
-async function displayAppliancesList(recipesList) {
-  const appliancesFilter = document.querySelector('#appliances-list')
-
-  let appliances = []
-  // list all appliances
-  for (let i = 0; i < recipesList.length; i++) {
-    appliances.push(recipesList[i].appliance)
-  }
-  // delete double
-  appliances = [...new Set(appliances)]
-  // sort
-  appliances.sort()
-
-  const appliancesModel = appliancesFactory(appliances)
-  const appliancesListDOM = appliancesModel.getAppliancesListDOM()
-  appliancesFilter.appendChild(appliancesListDOM)
-}
-
-async function displayUstensilsList(recipesList) {
-  const ustensilsFilter = document.querySelector('#ustensils-list')
-
-  let ustensils = []
-  // list all ustensils
-  for (let i = 0; i < recipesList.length; i++) {
-    recipesList[i].ustensils.forEach((e) => {
-      ustensils.push(e)
-    })
-  }
-  // delete double
-  ustensils = [...new Set(ustensils)]
-  // sort
-  ustensils.sort()
-
-  const ustensilsModel = ustensilsFactory(ustensils)
-  const ustensilsListDOM = ustensilsModel.getUstensilsListDOM()
-  ustensilsFilter.appendChild(ustensilsListDOM)
+  // ustensils list creation
+  const ustensilsList = await displayOptionsList(
+    recipesList,
+    '#ustensils-list',
+    'ustensiles'
+  )
 }
 
 async function init() {
   const recipesList = await getRecipes()
   // recipies cards creation
   const recipesCards = await displayRecipes(recipesList.recipesData)
-  // ingredients list creation
-  const ingredientsList = await displayIngredientsList(recipesList.recipesData)
-  // appliance list creation
-  const appliancesList = await displayAppliancesList(recipesList.recipesData)
-  // ustensils list creation
-  const ustensilsList = await displayUstensilsList(recipesList.recipesData)
+  // options lists creation
+  const optionsList = await displayOptions(recipesList.recipesData)
 
   return { recipesCards }
 }
